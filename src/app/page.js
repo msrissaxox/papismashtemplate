c2f5b35c932e88b50eab3ba6029c9f953693cb29
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-18 23:31:57 -0400

    add prettier

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,11 @@
-import Image from "next/image";
+//ADD PRETTIER
+//FIX MENU BAR
+//FADE IN FOR ALL SECTIONS
+//FIX ON THE MENU SECTION
+// FIX REVIEWS SECTION
+//FIX VISIT US SECTION
+
+// bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import OurStory from "@/components/OurStory";
@@ -10,15 +17,13 @@ import VisitUs from "@/components/VisitUs";
 export default function Home() {
   return (
     <div>
-<Header />
-<Hero />
-<OurStory />
-<Ingredients />
-<OnTheMenu />
-<Reviews />
-<VisitUs />
-
+      <Header />
+      <Hero />
+      <OurStory />
+      <Ingredients />
+      <OnTheMenu />
+      <Reviews />
+      <VisitUs />
     </div>
-
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 export default function Hero() {
-    return (
-        <div className="flex flex-col items-center justify-center bg-gradient-to-r from-gray-400 to-white text-white">
-            <h1 className="text-7xl font-bold pt-20 text-amber-600">Papi Smash'd</h1>
-            <h2 className="p-10">Delicious Food for Every Mood </h2>
-            <img src="papi1.png" alt="Papi Smash'd Logo" className="h-170 w-75%" />
-           
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center bg-gradient-to-r from-gray-400 to-white text-white">
+      <h1 className="text-7xl font-bold pt-20 bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent">
+        PAPI SMASH'D
+      </h1>
+      <h2 className="p-10 text-black text-2xl">
+        Delicious Food for Every Mood{" "}
+      </h2>
+      <img src="papi1.png" alt="Papi Smash'd Logo" className="h-170 w-75%" />
+    </div>
+  );
 }
